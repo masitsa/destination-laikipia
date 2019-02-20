05c5dd5d79e1ee764eecf464611f3f218971f2af
kwizl <karis.njoroge@gmail.com>
2019-02-20 15:23:39 +0300

    first commit

---
+++ DestinationLaikipia/DestinationLaikipia/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DestinationLaikipia.Models;
+
+namespace DestinationLaikipia.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
