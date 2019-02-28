d96e938e24a777300ca46db76ae975449943767b
fredthedeveloper <fredrickamau18@gmail.com>
2019-02-28 19:36:17 +0300

    created accommodationBooking.xaml

--- DestinationLaikipia/DestinationLaikipia.Android/Resources/Resource.designer.cs
+++ DestinationLaikipia/DestinationLaikipia.Android/Resources/Resource.designer.cs
@@ -4465,26 +4465,26 @@ namespace DestinationLaikipia.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02015b
-			public const int avd_hide_password_1 = 2130837851;
+			// aapt resource value: 0x7f02015e
+			public const int avd_hide_password_1 = 2130837854;
 			
-			// aapt resource value: 0x7f02015c
-			public const int avd_hide_password_2 = 2130837852;
+			// aapt resource value: 0x7f02015f
+			public const int avd_hide_password_2 = 2130837855;
 			
-			// aapt resource value: 0x7f02015d
-			public const int avd_hide_password_3 = 2130837853;
+			// aapt resource value: 0x7f020160
+			public const int avd_hide_password_3 = 2130837856;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02015e
-			public const int avd_show_password_1 = 2130837854;
+			// aapt resource value: 0x7f020161
+			public const int avd_show_password_1 = 2130837857;
 			
-			// aapt resource value: 0x7f02015f
-			public const int avd_show_password_2 = 2130837855;
+			// aapt resource value: 0x7f020162
+			public const int avd_show_password_2 = 2130837858;
 			
-			// aapt resource value: 0x7f020160
-			public const int avd_show_password_3 = 2130837856;
+			// aapt resource value: 0x7f020163
+			public const int avd_show_password_3 = 2130837859;
 			
 			// aapt resource value: 0x7f020055
 			public const int car_image = 2130837589;
@@ -5239,11 +5239,11 @@ namespace DestinationLaikipia.Droid
 			// aapt resource value: 0x7f02014f
 			public const int notification_icon_background = 2130837839;
 			
-			// aapt resource value: 0x7f020159
-			public const int notification_template_icon_bg = 2130837849;
+			// aapt resource value: 0x7f02015c
+			public const int notification_template_icon_bg = 2130837852;
 			
-			// aapt resource value: 0x7f02015a
-			public const int notification_template_icon_low_bg = 2130837850;
+			// aapt resource value: 0x7f02015d
+			public const int notification_template_icon_low_bg = 2130837853;
 			
 			// aapt resource value: 0x7f020150
 			public const int notification_tile_bg = 2130837840;
@@ -5261,16 +5261,25 @@ namespace DestinationLaikipia.Droid
 			public const int rhino = 2130837844;
 			
 			// aapt resource value: 0x7f020155
-			public const int shopping_mall = 2130837845;
+			public const int room1 = 2130837845;
 			
 			// aapt resource value: 0x7f020156
-			public const int splashscreen = 2130837846;
+			public const int room2 = 2130837846;
 			
 			// aapt resource value: 0x7f020157
-			public const int tooltip_frame_dark = 2130837847;
+			public const int room3 = 2130837847;
 			
 			// aapt resource value: 0x7f020158
-			public const int tooltip_frame_light = 2130837848;
+			public const int shopping_mall = 2130837848;
+			
+			// aapt resource value: 0x7f020159
+			public const int splashscreen = 2130837849;
+			
+			// aapt resource value: 0x7f02015a
+			public const int tooltip_frame_dark = 2130837850;
+			
+			// aapt resource value: 0x7f02015b
+			public const int tooltip_frame_light = 2130837851;
 			
 			static Drawable()
 			{
